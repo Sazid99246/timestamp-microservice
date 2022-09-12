8086076464f602ea085201f1c893432a39f9183b
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-09-12 08:15:01 +0600

    task 7 and 8 completed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,8 +18,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/:date', (req, res)=>{
+app.get('/api/:date', (req, res) => {
   const date = new Date(req.params.date);
+  const timeStampinMs = date.getTime();
+  const utc = date.toUTCString();
+  res.json({
+    unix: timeStampinMs,
+    utc: utc
+  })
+})
+
+app.get('/api', (req, res) => {
+  const date = new Date();
   const timeStampinMs = date.getTime();
   const utc = date.toUTCString();
   res.json({
@@ -30,7 +40,7 @@ app.get('/api/:date', (req, res)=>{
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
